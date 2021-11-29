641b084263462e2341ce472179e8e4a4d3f3e0d7
Paf <pavlinpenevv@abv.bg>
2021-11-29 20:34:15 +0200

    Initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from "./heroes/hero";
+
+export const HEROES: Hero[] = [
+    {id: 1, name: 'Pavkata'},
+    {id: 2, name: 'Marianski'},
+    {id: 3, name: 'Misho'},
+    {id: 4, name: 'Vladi'},
+    {id: 5, name: 'Fil'},
+    {id: 6, name: 'Dimi'},
+    {id: 7, name: 'Denis'},
+    {id: 8, name: 'Denislav'},
+    {id: 9, name: 'Krasi'},
+    {id: 10, name: 'Ivi'},
+    {id: 11, name: 'Aleks'},
+    {id: 12, name: 'Agi'},
+]
