0a9d29a1a0f1472a1730aeaaef02d2f27973bc0f
Paf <86779216+PavlinPenev@users.noreply.github.com>
2021-12-16 22:51:02 +0200

    Update in-memory-data.service.ts

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -24,7 +24,7 @@ export class InMemoryDataService implements InMemoryDbService {
 
   // Overrides the genId method to ensure that a hero always has an id.
   // If the heroes array is empty,
-  // the method below returns the initial number (11).
+  // the method below returns the initial number (1).
   // if the heroes array is not empty, the method below returns the highest
   // hero id + 1.
   genId(heroes: Hero[]): number {
